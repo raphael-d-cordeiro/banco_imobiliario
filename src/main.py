627752fb97c230157b021f732059791735b121d6
RAPHAEL D CORDEIRO <raphael.d.cordeiro@gmail.com>
2022-11-30 22:50:32 -0300

    add game classes

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+
+from board.game_statistics import show_statistics
+from board.factory import create_board
+
+
+load_dotenv()
+
+
+def main():
+    '''
+    Uma execução do programa proposto deve rodar 300
+    simulações, imprimindo no console os dados
+    referentes às execuções.
+    '''
+    results = []
+    for index in range(int(os.getenv('ENV_NUMBER_SIMULATION'))):
+        board = create_board()
+        while board.winner is None:
+            for player in board.players:
+                if player.gameover:
+                    board.remove(player)
+                winner = board.check_winner(player)
+                if winner:
+                    board.winner = winner
+                    break
+                board.play(player, board)
+            board.played += 1
+        results.append(board.finish())
+    show_statistics(results)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/players/player_random.py
@@ -0,0 +1,17 @@
+from random import randint
+
+from .base import BasePlayer
+
+
+class PlayerRandom(BasePlayer):
+    '''
+    O jogador aleatório compra a propriedade
+    que ele parar em cima com probabilidade
+    de 50%.
+    '''
+
+    def _rules_to_payment(self, patrimony):
+        if randint(0, 1) > 0:
+            self.paid(patrimony.property_price, patrimony.type_of_strategy)
+            return True
+        return False
